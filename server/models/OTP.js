2fb83c94153f1ea59ddb1b11ab38f078c872a820
Siddharth Patel <connectwithsiddh@gmail.com>
2025-07-29 22:13:17 +0530

    Added all backend models User, Profile, SubSection, Section, OTP, Course, CourseProgress, Cateogry, RatingAndReview. OTP schema, mail sender, and email verification template

---
+++ server/models/OTP.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+const mailSender = require("../utils/mailSender");
+const emailTemplate = require("../templates/emailVerificationTemplate");
+
+const otpSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    otp: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      expires: 5 * 60,
+    },
+  },
+  { timestamps: true }
+);
+
+// function to send emails
+async function sendVerificationEmail(email, otp) {
+  try {
+    const mailResponse = await mailSender(
+      email,
+      "Verification Email",
+      emailTemplate(otp)
+    );
+    console.log("Email sent successfully:", mailResponse.response);
+  } catch (error) {
+    console.log("Error occurred while sending email: ", error);
+    throw error;
+  }
+}
+
+// pre-save hook to send email after the document has been saved
+otpSchema.pre("save", async function (next) {
+  console.log("New Document saved to database");
+  if (this.isNew) {
+    await sendVerificationEmail(this.email, this.otp);
+  }
+  next();
+});
+
+module.exports = mongoose.model("OTP", otpSchema);
